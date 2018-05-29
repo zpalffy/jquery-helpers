fa4bd4190f72a19b254efcfcdc3d994d2a107bfa
zpalffy <zpalffy@gmail.com>
2018-05-29 16:51:50 -0400

    test changes, refactored parms to make testable

--- jquery-helpers.js
+++ jquery-helpers.js
@@ -2,9 +2,6 @@
   'use strict';
 
   $.extend({
-    // Usage: $.parms.name
-    parms: {}, // filled in later    
-
     // Usage: $.timeout(5000, "my string").done(...)
     // Usage: $.timeout(2000).done(...)
     // Usage: $.timeout(200).cancel() 
@@ -30,30 +27,39 @@
       return json ? $.when(JSON.parse(json)) : $.when(fillItemFx()).done(function(v) {
         opts.storage.setItem(itemKey, JSON.stringify(v));
       });
-    }
-  });
-  
-  // fill in $.parms
-  $.each($(window.location.search.substring(1).split('&')), function(i, pair) {
-    if (pair) {
-      pair = pair.split('=');
-      $.parms[pair[0]] = pair[1] ? window.decodeURIComponent(pair[1].replace(/\+/g, '%20')) : true;
+    },
+    
+    parseParameters: function(str) {
+      var parms = {};
+      $.each(str.substring(1).split('&'), function(i, pair) {
+        if (pair) {
+          pair = pair.split('=');
+          parms[pair[0]] = pair[1] ? window.decodeURIComponent(pair[1].replace(/\+/g, '%20')) : true;
+        }
+      });
+      
+      return parms;
     }
   });
 
+  // Usage: $.parms.name
+  $.parms = $.parseParameters(window.location.search);
+  
   // Usage: $('#template').handlebars({ name: 'fred', age: 41 })
   $.fn.handlebars = function(ctx, options) {
     var opts = $.extend({
       effect: function(newContent) {
-        newContent.slideDown();
+        return newContent.slideDown();
       }
     }, options);
 
-    return this.each(function() {
+   return this.each(function() {
       var content = $(Handlebars.compile(this.innerHTML)(ctx)).hide();
       $(this).replaceWith(content);
       content.trigger('contentAdded'); // but not displayed
-      opts.effect(content);
+      $.when(opts.effect(content)).done(function() {
+        content.trigger('contentShown');
+      });
     });
   };  
 }) (jQuery);
