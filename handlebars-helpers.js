611e7a68078534f7cd19b2fcdcadb46ddb1ebcbb
Eric <zpalffy@gmail.com>
2019-02-17 17:02:21 -0600

    added playlist, better async element for helpers?

--- handlebars-helpers.js
+++ handlebars-helpers.js
@@ -1,9 +1,13 @@
 (function(undefined) {
   'use strict';
 
-  var randomString = function() {
-    return Math.random().toString(36).substring(7);
-  };
+  var asyncElement = function() {
+    var id =  Math.random().toString(36).substring(7);
+    return {
+      find: () => document.getElementById(id);
+      element: '<span id="' + id + '"></span>'
+    };
+  }
 
   Handlebars.registerHelper('formatDate', function(ts, format) {
     return new Handlebars.SafeString(moment(ts).format(format));
@@ -18,14 +22,19 @@
       id + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
   });
 
+  Handlebars.registerHelper('youtube-playlist', function(id) {
+    return new Handlebars.SafeString('<iframe class="youtube embed" src="https://www.youtube.com/embed/videoseries?list=' + 
+      id + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+  });
+  
   Handlebars.registerHelper('viz', function(opts) {
-    var id = randomString();
+    var ele = asyncElement();
 
     new Viz().renderSVGElement(opts.fn()).then(
-      graph => document.getElementById(id).appendChild(graph), 
-      ex => document.getElementById(id).innerHTML = '<div class="error">Error in {{ viz }} helper: ' + ex + "</div>"
+      graph => ele.find().appendChild(graph), 
+      ex => ele.find().innerHTML = '<div class="error">Error in {{ viz }} helper: ' + ex + "</div>" // needs help to show as error
     );
 
-    return new Handlebars.SafeString('<span id="' + id + '"></span>');
+    return new Handlebars.SafeString(ele.element);
   });
 })();
